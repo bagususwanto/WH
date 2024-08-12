019cb1beda6e78fc7bfe4e13fcbae0108f8e3b04
Jhamezzzzzz <jhamesanderson2929@gmail.com>
2024-08-12 09:21:19 +0700

    update Category,bikin master category

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 const Inventory = React.lazy(() => import('./views/inventory/Inventory'))
-
+const Orderlist = React.lazy(() => import('./views/orderlist/Orderlist'))
+const Category = React.lazy(() => import('./views/masterdata/category/Category'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/inventory', name: 'Inventory', element: Inventory },
+  { path: '/category', name: 'Category', element: Category },
 ]
 
 export default routes
