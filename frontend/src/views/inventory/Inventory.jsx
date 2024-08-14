8dc73fbfc30e7d7343fe78894a6385eda334322a
bagususwanto <bagoes.1933@gmail.com>
2024-08-14 18:02:34 +0700

    mengganti tampilan tabel di halaman inventory menggunakan datatable primeReact

--- frontend/src/views/inventory/Inventory.jsx
+++ frontend/src/views/inventory/Inventory.jsx
@@ -1,44 +1,47 @@
-import React, { useState, useEffect } from 'react'
-import {
-  CButton,
-  CCard,
-  CCardHeader,
-  CCardBody,
-  CCol,
-  CRow,
-  CTable,
-  CTableHead,
-  CTableRow,
-  CTableHeaderCell,
-  CTableBody,
-  CCardFooter,
-  CTableDataCell,
-  CFormLabel,
-  CForm,
-  CFormSelect,
-} from '@coreui/react'
+import React, { useState, useEffect, useRef } from 'react'
+import { CButton, CCard, CCardHeader, CCardBody, CCol, CRow } from '@coreui/react'
 import axiosInstance from '../../utils/AxiosInstance'
-import Flatpickr from 'react-flatpickr'
-import 'flatpickr/dist/themes/dark.css'
-import CIcon from '@coreui/icons-react'
-import { cilDataTransferDown, cilZoom } from '@coreui/icons'
-import { func } from 'prop-types'
 import { DataTable } from 'primereact/datatable'
 import { Column } from 'primereact/column'
+import { Dropdown } from 'primereact/dropdown'
+import { FilterMatchMode } from 'primereact/api'
+import 'primeicons/primeicons.css'
+import { IconField } from 'primereact/iconfield'
+import { InputIcon } from 'primereact/inputicon'
+import { InputText } from 'primereact/inputtext'
+import { Button } from 'primereact/button'
+import 'primereact/resources/themes/mira/theme.css'
+import 'primereact/resources/primereact.min.css'
 
 const Inventory = () => {
   const [inventory, setInventory] = useState([])
   const [plant, setPlant] = useState([])
   const [shop, setShop] = useState([])
   const [location, setLocation] = useState([])
-  const [date, setDate] = useState(new Date())
+  const [loading, setLoading] = useState(true)
+  const [globalFilterValue, setGlobalFilterValue] = useState('')
+  const [visibleData, setVisibleData] = useState([]) // Data yang terlihat di tabel
+
+  const [filters, setFilters] = useState({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    'Material.Address_Rack.Location.locationName': {
+      value: null,
+      matchMode: FilterMatchMode.EQUALS,
+    },
+  })
+
+  const dt = useRef(null)
 
   useEffect(() => {
-    // getInventory()
+    getInventory()
     getPlant()
-    // getShop()
-    // getLocation()
+    getLocation()
+    setLoading(false)
   }, [])
+
+  useEffect(() => {
+    applyFilters()
+  }, [filters, globalFilterValue, inventory])
 
   const getInventory = async () => {
     try {
@@ -58,214 +61,172 @@ const Inventory = () => {
     }
   }
 
-  const getShop = async () => {
-    try {
-      const response = await axiosInstance.get('/shop')
-      setShop(response.data)
-    } catch (error) {
-      console.error('Error fetching shop:', error)
-    }
-  }
-
-  const getShopByPlantId = async (id) => {
-    try {
-      const response = await axiosInstance.get(`/shop-plant/${id}`)
-      setShop(response.data)
-    } catch (error) {
-      console.error('Error fetching shop by ID:', error)
-    }
-  }
-
   const getLocation = async () => {
     try {
       const response = await axiosInstance.get('/location')
-      setLocation(response.data)
+      const locationNames = response.data.map((loc) => loc.locationName)
+      setLocation(locationNames)
     } catch (error) {
       console.error('Error fetching location:', error)
     }
   }
 
-  const handleSearch = () => {
-    getInventory()
+  const handleLocationChange = (e) => {
+    const value = e.value
+    let _filters = { ...filters }
+    _filters['Material.Address_Rack.Location.locationName'].value = value
+    setFilters(_filters)
   }
 
-  const handlePlantChange = (id) => {
-    getShopByPlantId(id)
+  const onGlobalFilterChange = (e) => {
+    const value = e.target.value
+    let _filters = { ...filters }
+    _filters['global'].value = value
+    setFilters(_filters)
+    setGlobalFilterValue(value)
+  }
+
+  const applyFilters = () => {
+    let filteredData = [...inventory]
+
+    if (filters['global'].value) {
+      filteredData = filteredData.filter((item) =>
+        Object.values(item).some((val) =>
+          String(val).toLowerCase().includes(filters['global'].value.toLowerCase()),
+        ),
+      )
+    }
+
+    if (filters['Material.Address_Rack.Location.locationName'].value) {
+      filteredData = filteredData.filter(
+        (item) =>
+          item.Material.Address_Rack.Location.locationName ===
+          filters['Material.Address_Rack.Location.locationName'].value,
+      )
+    }
+
+    setVisibleData(filteredData)
+  }
+
+  const renderHeader = () => {
+    return (
+      <div className="d-flex align-items-center justify-content-end">
+        <IconField iconPosition="left">
+          <InputIcon className="pi pi-search" />
+          <InputText
+            value={globalFilterValue}
+            onChange={onGlobalFilterChange}
+            placeholder="Keyword Search"
+          />
+        </IconField>
+      </div>
+    )
+  }
+
+  const exportExcel = () => {
+    import('xlsx').then((xlsx) => {
+      // Mapping data untuk ekspor
+      const mappedData = visibleData.map((item) => ({
+        'Material No': item.Material.materialNo,
+        Description: item.Material.description,
+        Address: item.Material.Address_Rack.addressRackName,
+        UOM: item.Material.uom,
+        Plant: item.Material.Address_Rack.Location.Shop.Plant.plantName,
+        Location: item.Material.Address_Rack.Location.locationName,
+      }))
+
+      const worksheet = xlsx.utils.json_to_sheet(mappedData)
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] }
+      const excelBuffer = xlsx.write(workbook, {
+        bookType: 'xlsx',
+        type: 'array',
+      })
+
+      saveAsExcelFile(excelBuffer, 'inventory')
+    })
+  }
+
+  const saveAsExcelFile = (buffer, fileName) => {
+    import('file-saver').then((module) => {
+      if (module && module.default) {
+        let EXCEL_TYPE =
+          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8'
+        let EXCEL_EXTENSION = '.xlsx'
+        const data = new Blob([buffer], {
+          type: EXCEL_TYPE,
+        })
+
+        module.default.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION)
+      }
+    })
   }
 
   return (
     <CRow>
       <CCol>
         <CCard className="mb-3">
-          <CCardHeader>Search</CCardHeader>
+          <CCardHeader>Filter</CCardHeader>
           <CCardBody>
-            <CForm>
-              <CRow>
-                <CCol md={3} className="mb-3">
-                  <CFormLabel htmlFor="dateInput">Date</CFormLabel>
-                  <Flatpickr
-                    value={date}
-                    onChange={(date) => setDate(date)}
-                    options={{
-                      dateFormat: 'Y-m-d',
-                      maxDate: 'today',
-                      allowInput: true,
-                    }}
-                    className="form-control"
-                    placeholder="Select a date"
-                  />
-                </CCol>
-                <CCol md={2} className="mb-3">
-                  <CFormLabel htmlFor="plantinput">Plant</CFormLabel>
-                  <CFormSelect
-                    aria-label="Select a plant"
-                    onChange={(e) => handlePlantChange(e.target.value)}
-                  >
-                    <option>Select a plant</option>
-                    {plant.map((plant) => (
-                      <option key={plant.id} value={plant.id}>
-                        {plant.plantName}
-                      </option>
-                    ))}
-                  </CFormSelect>
-                </CCol>
-                <CCol md={2} className="mb-3">
-                  <CFormLabel htmlFor="shopinput">Shop</CFormLabel>
-                  <CFormSelect aria-label="Select a plant">
-                    <option>Select a shop</option>
-                    {shop.map((shop) => (
-                      <option key={shop.id} value={shop.id}>
-                        {shop.shopName}
-                      </option>
-                    ))}
-                  </CFormSelect>
-                </CCol>
-                <CCol md={3} className="mb-3">
-                  <CFormLabel htmlFor="locationinput">Location</CFormLabel>
-                  <CFormSelect aria-label="Select a plant">
-                    <option>Select a location</option>
-                    {location.map((location) => (
-                      <option key={location.id} value={location.id}>
-                        {location.locationName}
-                      </option>
-                    ))}
-                  </CFormSelect>
-                </CCol>
-                <CCol md={2} className="mb-3">
-                  <CFormLabel htmlFor="shiftinput">Shift</CFormLabel>
-                  <CFormSelect aria-label="Select a plant">
-                    <option>Select a shift</option>
-                    <option value={1}>Day</option>
-                    <option value={2}>Night</option>
-                  </CFormSelect>
-                </CCol>
-              </CRow>
-            </CForm>
-          </CCardBody>
-          <CCardFooter>
-            <CRow className="align-items-end">
-              <CCol className="d-flex justify-content-end">
-                <CButton color="dark" onClick={handleSearch}>
-                  <CIcon icon={cilZoom} /> Search
-                </CButton>
+            <CRow>
+              <CCol md={4}>
+                <Dropdown
+                  value={filters['Material.Address_Rack.Location.locationName'].value}
+                  options={location}
+                  onChange={handleLocationChange}
+                  placeholder="Select Location"
+                  className="p-column-filter"
+                  showClear
+                  style={{ minWidth: '12rem' }}
+                />
               </CCol>
             </CRow>
-          </CCardFooter>
+          </CCardBody>
         </CCard>
 
         <CCard className="mb-3">
           <CCardHeader>Inventory Table</CCardHeader>
           <CCardBody>
-            <CRow>
-              <CCol className="mb-1">
-                <CButton color="success">
-                  <CIcon icon={cilDataTransferDown} /> Excel
-                </CButton>
+            <CRow className="mb-2">
+              <CCol md={3}>
+                <Button
+                  type="button"
+                  label="Excel"
+                  icon="pi pi-file-excel"
+                  severity="success"
+                  className="rounded-5"
+                  onClick={exportExcel}
+                  data-pr-tooltip="XLS"
+                />
               </CCol>
+              <CCol>{renderHeader()}</CCol>
             </CRow>
-            <DataTable value={inventory} tableStyle={{ minWidth: '50rem' }}>
-              <Column field="materialNo" header="materialNo"></Column>
-              <Column field="description" header="description"></Column>
-              <Column field="address" header="address"></Column>
-              <Column field="uom" header="uom"></Column>
+            <DataTable
+              ref={dt}
+              value={visibleData}
+              tableStyle={{ minWidth: '50rem' }}
+              className="p-datatable-gridlines p-datatable-sm custom-datatable"
+              paginator
+              rowsPerPageOptions={[10, 50, 100, 500, 1000]}
+              rows={10}
+              dataKey="id"
+              filters={filters}
+              loading={loading}
+              emptyMessage="No inventory found."
+              size="small"
+            >
+              <Column field="Material.materialNo" header="Material No"></Column>
+              <Column field="Material.description" header="Description"></Column>
+              <Column field="Material.Address_Rack.addressRackName" header="Address"></Column>
+              <Column field="Material.uom" header="UoM"></Column>
+              <Column
+                field="Material.Address_Rack.Location.Shop.Plant.plantName"
+                header="Plant"
+              ></Column>
+              <Column
+                field="Material.Address_Rack.Location.locationName"
+                header="Location"
+              ></Column>
             </DataTable>
-            <CTable small bordered responsive className="text-nowrap">
-              <CTableHead color="light">
-                <CTableRow>
-                  <CTableHeaderCell rowSpan="2" scope="col" className="text-center align-middle">
-                    No
-                  </CTableHeaderCell>
-                  <CTableHeaderCell rowSpan="2" scope="col" className="text-center align-middle">
-                    Item No
-                  </CTableHeaderCell>
-                  <CTableHeaderCell rowSpan="2" scope="col" className="text-center align-middle">
-                    Material Description
-                  </CTableHeaderCell>
-                  <CTableHeaderCell rowSpan="2" scope="col" className="text-center align-middle">
-                    Address
-                  </CTableHeaderCell>
-                  <CTableHeaderCell rowSpan="2" scope="col" className="text-center align-middle">
-                    UoM
-                  </CTableHeaderCell>
-                  <CTableHeaderCell colSpan="2" className="text-center align-middle">
-                    Incoming
-                  </CTableHeaderCell>
-                  <CTableHeaderCell rowSpan="2" scope="col" className="text-center align-middle">
-                    Good Issue
-                  </CTableHeaderCell>
-                  <CTableHeaderCell rowSpan="2" scope="col" className="text-center align-middle">
-                    Standar Stock
-                  </CTableHeaderCell>
-                  <CTableHeaderCell colSpan="2" scope="col" className="text-center align-middle">
-                    Stock
-                  </CTableHeaderCell>
-                  <CTableHeaderCell rowSpan="2" scope="col" className="text-center align-middle">
-                    Judgement
-                  </CTableHeaderCell>
-                  <CTableHeaderCell rowSpan="2" scope="col" className="text-center align-middle">
-                    User
-                  </CTableHeaderCell>
-                  <CTableHeaderCell rowSpan="2" scope="col" className="text-center align-middle">
-                    Date
-                  </CTableHeaderCell>
-                </CTableRow>
-                <CTableRow>
-                  <CTableHeaderCell scope="col" className="text-center align-middle">
-                    Plan
-                  </CTableHeaderCell>
-                  <CTableHeaderCell scope="col" className="text-center align-middle">
-                    Actual
-                  </CTableHeaderCell>
-                  <CTableHeaderCell scope="col" className="text-center align-middle">
-                    Sistem
-                  </CTableHeaderCell>
-                  <CTableHeaderCell scope="col" className="text-center align-middle">
-                    Actual
-                  </CTableHeaderCell>
-                </CTableRow>
-              </CTableHead>
-              <CTableBody>
-                {inventory.map((inventory, index) => (
-                  <CTableRow key={inventory.id}>
-                    <CTableHeaderCell scope="row">{index + 1}</CTableHeaderCell>
-                    <CTableDataCell>{inventory.Material.materialNo}</CTableDataCell>
-                    <CTableDataCell>{inventory.Material.description}</CTableDataCell>
-                    <CTableDataCell>
-                      {inventory.Material.Address_Rack.addressRackName}
-                    </CTableDataCell>
-                    <CTableDataCell>{inventory.Material.uom}</CTableDataCell>
-                    <CTableDataCell></CTableDataCell>
-                    <CTableDataCell></CTableDataCell>
-                    <CTableDataCell></CTableDataCell>
-                    <CTableDataCell></CTableDataCell>
-                    <CTableDataCell></CTableDataCell>
-                    <CTableDataCell></CTableDataCell>
-                    <CTableDataCell></CTableDataCell>
-                    <CTableDataCell></CTableDataCell>
-                  </CTableRow>
-                ))}
-              </CTableBody>
-            </CTable>
           </CCardBody>
         </CCard>
       </CCol>
