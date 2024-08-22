ef69f5394dc4de499d31533f0b17c7f2b9cf0a7f
bagususwanto <bagoes.1933@gmail.com>
2024-08-22 15:39:30 +0700

    fix bug addressRack api

--- backend/controllers/AddressRack.js
+++ backend/controllers/AddressRack.js
@@ -15,7 +15,7 @@ export const getAddressRack = async (req, res) => {
           include: [
             {
               model: Shop,
-              attributes: ["id", "shopName", "costCenter", "wbsNumber", "ext", "createdAt", "updatedAt"],
+              attributes: ["id", "shopName", "createdAt", "updatedAt"],
               include: [
                 {
                   model: Plant,
@@ -60,7 +60,7 @@ export const getAddressRackById = async (req, res) => {
           include: [
             {
               model: Shop,
-              attributes: ["id", "shopName", "costCenter", "wbsNumber", "ext", "createdAt", "updatedAt"],
+              attributes: ["id", "shopName", "createdAt", "updatedAt"],
               include: [
                 {
                   model: Plant,
