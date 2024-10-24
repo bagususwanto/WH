71928a73122b09fa7936c445af6dcae8a84d7b92
bagususwanto <bagoes.1933@gmail.com>
2024-10-24 17:59:50 +0700

    "Refactored AppContent and AppHeader components, updated routes and navigation logic, and removed unused imports and code."

--- frontend/src/components/AppContent.jsx
+++ frontend/src/components/AppContent.jsx
@@ -1,33 +1,59 @@
 import React, { Suspense } from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import { CContainer, CSpinner } from '@coreui/react'
 
 // routes config
 import routes from '../routes'
 
 const AppContent = () => {
+  const location = useLocation()
+
   return (
-    <CContainer className="px-4" fluid>
-      <Suspense fallback={<CSpinner color="primary" />}>
-        <Routes>
-          {routes.map((route, idx) => {
-            return (
-              route.element && (
-                <Route
-                  key={idx}
-                  path={route.path}
-                  exact={route.exact}
-                  name={route.name}
-                  element={<route.element />}
-                />
+    <>
+      {location.pathname !== '/home' ? (
+        <CContainer className="px-4" fluid>
+          <Suspense fallback={<CSpinner color="primary" />}>
+            <Routes>
+              {routes.map((route, idx) => {
+                return (
+                  route.element && (
+                    <Route
+                      key={idx}
+                      path={route.path}
+                      exact={route.exact}
+                      name={route.name}
+                      element={<route.element />}
+                    />
+                  )
+                )
+              })}
+              <Route path="/" element={<Navigate to="home" replace />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </Suspense>
+        </CContainer>
+      ) : (
+        <Suspense fallback={<CSpinner color="primary" />}>
+          <Routes>
+            {routes.map((route, idx) => {
+              return (
+                route.element && (
+                  <Route
+                    key={idx}
+                    path={route.path}
+                    exact={route.exact}
+                    name={route.name}
+                    element={<route.element />}
+                  />
+                )
               )
-            )
-          })}
-          <Route path="/" element={<Navigate to="home" replace />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </Suspense>
-    </CContainer>
+            })}
+            <Route path="/" element={<Navigate to="home" replace />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Suspense>
+      )}
+    </>
   )
 }
 
--- frontend/src/components/AppHeader.jsx
+++ frontend/src/components/AppHeader.jsx
@@ -1,45 +1,27 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { useNavigate } from 'react-router-dom' // Import useNavigate
+import { useNavigate, useLocation } from 'react-router-dom' // Import useLocation
 import { useSelector, useDispatch } from 'react-redux'
 import {
   CContainer,
   CDropdown,
   CCol,
-  CDropdownHeader,
-  CDropdownItem,
-  CDropdownMenu,
   CDropdownToggle,
   CHeader,
   CHeaderNav,
   CHeaderToggler,
-  CNavLink,
-  CNavItem,
-  useColorModes,
-  CBadge,
 } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import {
-  cilAsteriskCircle,
-  cilBell,
-  cilCart,
-  cilCheck,
-  cilClearAll,
-  cilContrast,
-  cilMenu,
-  cilMoon,
-  cilSun,
-} from '@coreui/icons'
+import { CIcon } from '@coreui/icons-react'
+import { cilBell, cilMenu } from '@coreui/icons'
 
 import { AppBreadcrumb } from './index'
 import { AppHeaderDropdown } from './header/index'
 
 const AppHeader = () => {
   const headerRef = useRef()
-  const { colorMode, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
-
+  const location = useLocation() // Get the current location
   const dispatch = useDispatch()
   const sidebarShow = useSelector((state) => state.sidebarShow)
-  const navigate = useNavigate() // Inisialisasi useNavigate
+  const navigate = useNavigate()
 
   const [isDropdownOpen, setIsDropdownOpen] = useState(false)
 
@@ -60,126 +42,46 @@ const AppHeader = () => {
     } else {
       dropdownElement?.classList.add('sticky-search-bar')
     }
-    setIsDropdownOpen(!isDropdownOpen) // Toggle state
+    setIsDropdownOpen(!isDropdownOpen)
   }
 
   const notificationCount = 3
 
+  // Conditionally apply 'mb-4' based on the current path
+  const headerClass = location.pathname === '/home' ? 'mb-1 p-0' : 'mb-4 p-0'
+
   return (
-    <CHeader position="sticky" className="mb-4 p-0" ref={headerRef}>
+    <CHeader position="sticky" className={headerClass} ref={headerRef}>
       <CContainer className="border-bottom px-1" fluid>
-        <CCol xs ={1}>
-          <CHeaderToggler
-            onClick={() => dispatch({ type: 'set', sidebarShow: !sidebarShow })}
-           
-          >
+        <CCol xs={1}>
+          <CHeaderToggler onClick={() => dispatch({ type: 'set', sidebarShow: !sidebarShow })}>
             <CIcon icon={cilMenu} size="lg" />
           </CHeaderToggler>
         </CCol>
-        <CCol xs={1} className='px-1'>
+        <CCol xs={1} className="px-1">
           <a href="/" className="d-flex align-items-center">
-            <img
-              src="/src/assets/brand/TWIIS-NEW.png"
-              alt="Logo"
-              style={{ height: '40px' }}
-            />
+            <img src="/src/assets/brand/TWIIS-NEW.png" alt="Logo" style={{ height: '40px' }} />
           </a>
         </CCol>
         <CCol sm={2}></CCol>
         <CHeaderNav className="d-none d-md-flex"></CHeaderNav>
         <CHeaderNav className="ms-auto">
-          <CDropdown
-            variant="nav-item"
-            onClick={handleDropdownToggle} // Set onClick to toggle dropdown
-          >
-            <CDropdownToggle
-              placement="bottom-end"
-              className="py-0 pe-0 d-flex align-items-center"
-              caret={false}
-            >
-              <CIcon
-                icon={cilBell}
-                size="lg"
-                style={{ marginRight: '0.5rem' }} // Atur margin untuk pengaturan jarak
-              />
-              {/* {notificationCount > 0 && (
-                <CBadge
-                  color="danger"
-                  shape="rounded-pill"
-                  className="position-absolute translate-middle"
-                  style={{
-                    right: '-10px',
-                    padding: '0.35em 0.6em',
-                    fontSize: '0.65rem',
-                  }}
-                >
-                  {notificationCount}
-                </CBadge>
-              )} */}
+          <CDropdown variant="nav-item" onClick={handleDropdownToggle}>
+            <CDropdownToggle className="py-0 pe-0 d-flex align-items-center" caret={false}>
+              <CIcon icon={cilBell} size="lg" style={{ marginRight: '0.5rem' }} />
             </CDropdownToggle>
-            <CDropdownMenu className="pt-0" placement="bottom-end">
-              {/* <CDropdownHeader className="bg-body-secondary fw-semibold my-2">
-                Anda memiliki ({notificationCount}) notifikasi
-              </CDropdownHeader>
-              <CDropdownItem href="#">Notifikasi 1</CDropdownItem>
-              <CDropdownItem href="#">Notifikasi 2</CDropdownItem>
-              <CDropdownItem href="#">Notifikasi 3</CDropdownItem> */}
-            </CDropdownMenu>
+            {/* Add dropdown menu here if necessary */}
           </CDropdown>
         </CHeaderNav>
         <CHeaderNav>
-          <li className="nav-item py-1">
-            <div className="vr h-100 mx-2 text-body text-opacity-75"></div>
-          </li>
-          {/* <CDropdown variant="nav-item" placement="bottom-end">
-            <CDropdownToggle caret={false}>
-              {colorMode === 'dark' ? (
-                <CIcon icon={cilMoon} size="lg" />
-              ) : colorMode === 'auto' ? (
-                <CIcon icon={cilContrast} size="lg" />
-              ) : (
-                <CIcon icon={cilSun} size="lg" />
-              )}
-            </CDropdownToggle>
-            <CDropdownMenu>
-              <CDropdownItem
-                active={colorMode === 'light'}
-                className="d-flex align-items-center"
-                as="button"
-                type="button"
-                onClick={() => setColorMode('light')}
-              >
-                <CIcon className="me-2" icon={cilSun} size="lg" /> Light
-              </CDropdownItem>
-              <CDropdownItem
-                active={colorMode === 'dark'}
-                className="d-flex align-items-center"
-                as="button"
-                type="button"
-                onClick={() => setColorMode('dark')}
-              >
-                <CIcon className="me-2" icon={cilMoon} size="lg" /> Dark
-              </CDropdownItem>
-              <CDropdownItem
-                active={colorMode === 'auto'}
-                className="d-flex align-items-center"
-                as="button"
-                type="button"
-                onClick={() => setColorMode('auto')}
-              >
-                <CIcon className="me-2" icon={cilContrast} size="lg" /> Auto
-              </CDropdownItem>
-            </CDropdownMenu>
-          </CDropdown>
-          <li className="nav-item py-1">
-            <div className="vr h-100 mx-2 text-body text-opacity-75"></div>
-          </li> */}
           <AppHeaderDropdown />
         </CHeaderNav>
       </CContainer>
-      <CContainer className="px-4" fluid>
-        <AppBreadcrumb />
-      </CContainer>
+      {location.pathname !== '/home' && (
+        <CContainer className="px-4" fluid>
+          <AppBreadcrumb />
+        </CContainer>
+      )}
     </CHeader>
   )
 }
