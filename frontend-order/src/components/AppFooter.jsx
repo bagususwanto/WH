1a556b09606a3e67a3b21bfb9fd9b14467c364fd
Jhamezzzzzz <jhamesanderson2929@gmail.com>
2024-11-04 17:29:34 +0700

    history change fuction

--- frontend-order/src/components/AppFooter.jsx
+++ frontend-order/src/components/AppFooter.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { CFooter } from '@coreui/react'
+import { useLocation } from 'react-router-dom'
 
 const AppFooter = () => {
+  const location = useLocation()
+
+  // Check if the current path is the cart screen
+  const isCartScreen = location.pathname.includes('/cart') // Adjust based on your actual cart path
+
   return (
-    <CFooter className="px-4">
-      <div>
-        <span className="ms-1">&copy; 2024 DX Warehouse.</span>
-      </div>
+    <CFooter 
+      className={`px-4 ${isCartScreen ? 'custom-footer' : ''}`}
+      style={isCartScreen ? { backgroundColor: 'white', height: '55px' } : {}}
+    >
+      {!isCartScreen && (
+        <div>
+          <span className="ms-1">&copy; 2024 DX Warehouse.</span>
+        </div>
+      )}
     </CFooter>
   )
 }
