904cde75b0b8696bdc03bf055e77643cb26aa241
@bagus_uswanto <bagoes.1933@gmail.com>
2024-11-22 11:30:45 +0700

    Added `limit: 1` to Plant model options in `getSectionByPlant` function.

--- backend/controllers/Section.js
+++ backend/controllers/Section.js
@@ -119,6 +119,7 @@ export const getSectionByPlant = async (req, res) => {
             {
               model: Plant,
               required: true,
+              limit: 1,
               attributes: ["id"],
               where: { id: plantId, flag: 1 },
             },
