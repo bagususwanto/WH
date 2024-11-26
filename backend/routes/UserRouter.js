a01f73cfe0a9dd4028baeba6313df922ac6092c8
@bagus_uswanto <bagoes.1933@gmail.com>
2024-11-26 08:23:31 +0700

    "Added getUser route and reformatted imports in UserRouter.js"

--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { getUser, getUserById, createUser, updateUser, deleteUser, createUserAndOrg } from "../controllers/User.js";
+import {
+  getUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  createUserAndOrg,
+} from "../controllers/User.js";
 import { checkRole } from "../middleware/RoleMiddleware.js";
 
 const router = express.Router();
@@ -10,5 +17,6 @@ router.post("/user", checkRole(["super admin"]), createUser);
 router.put("/user/:id", checkRole(["super admin"]), updateUser);
 router.get("/user-delete/:id", checkRole(["super admin"]), deleteUser);
 router.post("/user-org", checkRole(["super admin"]), createUserAndOrg);
+router.get("/user-public", getUser);
 
 export default router;
