6f27be09c961d9f73fe15e0d0dc7c1d53137441b
@bagus_uswanto <bagoes.1933@gmail.com>
2025-01-06 15:00:35 +0700

    `Update ChartRouter.js and _nav.jsx to include warehouse staff and group head roles`

--- backend/routes/ChartRouter.js
+++ backend/routes/ChartRouter.js
@@ -4,6 +4,20 @@ import { checkRole } from "../middleware/RoleMiddleware.js";
 
 const router = express.Router();
 
-router.get("/inventory-dashboard", checkRole(["super admin", "line head", "section head", "department head"]), getInventoryDashboard);
+router.get(
+  "/inventory-dashboard",
+  checkRole(
+    [
+      "super admin",
+      "warehouse staff",
+      "group head",
+      "line head",
+      "section head",
+      "department head",
+    ],
+    [1]
+  ),
+  getInventoryDashboard
+);
 
 export default router;
