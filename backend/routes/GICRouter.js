8b12a86880823abb311a52002538bc059d6816b6
@bagus_uswanto <bagoes.1933@gmail.com>
2025-02-25 16:38:41 +0700

    feat: add public endpoint for retrieving GIC data

--- backend/routes/GICRouter.js
+++ backend/routes/GICRouter.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { getGIC, getGICById, createGIC, updateGIC, deleteGIC } from "../controllers/GIC.js";
+import {
+  getGIC,
+  getGICById,
+  createGIC,
+  updateGIC,
+  deleteGIC,
+} from "../controllers/GIC.js";
 import { checkRole } from "../middleware/RoleMiddleware.js";
 
 const router = express.Router();
 
 router.get("/gic", checkRole(["super admin"]), getGIC);
+router.get("/gic-public", getGIC);
 router.get("/gic/:id", checkRole(["super admin"]), getGICById);
 router.post("/gic", checkRole(["super admin"]), createGIC);
 router.put("/gic/:id", checkRole(["super admin"]), updateGIC);
